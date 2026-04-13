d96c62b8093e07cf6a9b2e353e9a2519d5b3f9e4
Artem Priupolin <priupolin5443@yandex.ru>
2026-04-13 11:59:39 +0300

    feat: Added Sigil inventory and tooltip

---
+++ Assets/_Project/Scripts/SigilInventoryBar.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class SigilInventoryBar : MonoBehaviour
+{
+    [SerializeField] private Transform _slotContainer;
+    [SerializeField] private GameObject _slotPrefab;
+
+    private void OnEnable()
+    {
+        Refresh();
+    }
+
+    public void Refresh()
+    {
+        foreach (Transform child in _slotContainer)
+            Destroy(child.gameObject);
+
+        var sigils = GameManager.Instance?.Run.ActiveSigils ?? new List<Sigil>();
+
+        foreach (var sigil in sigils)
+        {
+            var go = Instantiate(_slotPrefab, _slotContainer);
+            go.GetComponent<SigilInventorySlot>().Setup(sigil);
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/SigilInventorySlot.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using TMPro;
+
+public class SigilInventorySlot : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] private TMP_Text _nameText;
+    [SerializeField] private GameObject _tooltip;
+    [SerializeField] private TMP_Text _tooltipName;
+    [SerializeField] private TMP_Text _tooltipType;
+    [SerializeField] private TMP_Text _tooltipDesc;
+
+    private Sigil _sigil;
+
+    public void Setup(Sigil sigil)
+    {
+        _sigil = sigil;
+        if (_nameText) _nameText.text = sigil.Name;
+        if (_tooltip) _tooltip.SetActive(false);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (_tooltip == null) return;
+        _tooltip.SetActive(true);
+        if (_tooltipName) _tooltipName.text = _sigil.Name;
+        if (_tooltipType) _tooltipType.text = _sigil.Type.ToFriendlyString();
+        if (_tooltipDesc) _tooltipDesc.text = _sigil.Description;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (_tooltip) _tooltip.SetActive(false);
+    }
+}
